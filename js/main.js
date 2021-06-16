58397604765da91f53c7fe52cbd18d42ec4f2203
aslan602 <jos18002@byui.edu>
2021-06-16 16:06:09 -0700

    Week 9

--- js/main.js
+++ js/main.js
@@ -26,7 +26,11 @@ const links = [
     {
         label: "Week 8",
         url: "week8/week8.html"
-    },               
+    },
+    {
+        label: "Week 9",
+        url: "week9/week9.html"
+    },                    
 ];
 
 function display() {    
